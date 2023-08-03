a85373fa34f8ce5160aca956b851e76ea9332d1b
Ryan Hall <dillbc@protonmail.com>
2023-08-03 11:29:15 -0400

    used RegExpressions to condense functions and retained testing feasability

--- src/piglatin.js
+++ src/piglatin.js
@@ -26,11 +26,6 @@ const consonants = [
 ];
 const vowels = ["a", "e", "i", "o", "u"];
 
-// searches for numbers (global)
-const numbers = /[0-9]/g;
-const regex = /[0-9@]/g;
-// searches for non letter characters (global)
-const symbols = /\W/g;
 // const splitToPush = () => {
 
 // }
@@ -78,22 +73,42 @@ const symbols = /\W/g;
 //   }
 // };
 
+// const translate = (word) => {
+//   word = word.toLowerCase();
+//   if (/[0-9@&$#!]/.test(word)) {
+//     return word;
+//   } else if (/[aeiou]/.test(word[0])) {
+//     return word + "way";
+//   } else {
+//     const wordArr = word.split("");
+//     const index = wordArr.findIndex((char) => /[aeiou]/.test(char));
+//     return word.slice(index) + word.slice(0, index) + "ay";
+//   }
+// };
+
+// searches for numbers (global)
+const numbers = /[0-9]/g;
+const regex = /[0-9@]/g;
+// searches for non letter characters (global)
+const symbols = /\W/g;
+
 const translate = (word) => {
-  const checkIfTwo = word.split(" ");
-  if (regex.test(word)) {
+  // const checkIfTwo = word.split(" ");
+  if (/[0-9@]/.test(word)) {
     return word;
   } else if (vowels.includes(word[0])) {
     return word.toLowerCase() + "way";
-  } else if (checkIfTwo.length > 1) {
+  } else if (word.split(" ").length > 1) {
+    let splitWord = word.split(" ");
     let preJoinArray = [];
-    for (let i = 0; i < checkIfTwo.length; i++) {
-      checkIfTwo[i].split("");
-      const index = checkIfTwo[i].search(vowels);
-      const conson = checkIfTwo[i].slice(index);
-      const vowelsPlus = checkIfTwo[i].slice(0, index);
+    for (let i = 0; i < splitWord.length; i++) {
+      splitWord[i].split("");
+      const index = splitWord[i].search(vowels);
+      const conson = splitWord[i].slice(index);
+      const vowelsPlus = splitWord[i].slice(0, index);
       preJoinArray.push(`${conson}${vowelsPlus}ay`);
     }
-    return preJoinArray.join(" ");
+    return preJoinArray.join(" ").toLowerCase();
   } else {
     const wordArray = word.split("");
     const index = wordArray.findIndex((char) => vowels.includes(char));
@@ -105,6 +120,25 @@ const translate = (word) => {
   }
 };
 
+const regExTranslate = (dialogue) => {
+  const numSymb = /[0-9@]/;
+  const vowels = /[aeiou]/i;
+  const pigArray = [];
+  const wordArr = dialogue.split(" ");
+  wordArr.forEach((word) => {
+    if (numSymb.test(word)) {
+      pigArray.push(word);
+    } else if (vowels.test(word[0])) {
+      pigArray.push(word + "way");
+    } else {
+      const index = word.search(vowels);
+      pigArray.push(word.slice(index) + word.slice(0, index) + "ay");
+    }
+  });
+  return pigArray.join(" ");
+};
+
 module.exports = {
   translate,
+  regExTranslate,
 };
--- tests/piglatin.test.js
+++ tests/piglatin.test.js
@@ -1,4 +1,4 @@
-const { translate } = require("../src/piglatin");
+const { translate, regExTranslate } = require("../src/piglatin");
 
 describe("translate function", () => {
   test("words that start with vowels", () => {
@@ -34,10 +34,55 @@ describe("translate function", () => {
     expect(translate("dill@gmail")).toBe("dill@gmail");
   });
 
+  // test("contains a space", () => {
+  //   expect(translate("Have a wonderful day")).toBe(
+  //     "avehay away onderfulway ayday"
+  //   );
+  //   expect(translate("good golly apple")).toBe("oodgay ollygay appleway");
+  // });
+});
+
+describe("regExpFunction", () => {
+  test("words that start with a vowel", () => {
+    expect(regExTranslate("apple")).toBe("appleway");
+    expect(regExTranslate("expect")).toBe("expectway");
+    expect(regExTranslate("ink")).toBe("inkway");
+    expect(regExTranslate("ocean")).toBe("oceanway");
+    expect(regExTranslate("under")).toBe("underway");
+  });
+  test("words that start with one consonant", () => {
+    expect(regExTranslate("cooler")).toBe("oolercay");
+    expect(regExTranslate("dingo")).toBe("ingoday");
+  });
+  test("words that start with two consonants", () => {
+    expect(regExTranslate("skills")).toBe("illsskay");
+  });
+  test("starts w three consonants", () => {
+    expect(regExTranslate("school")).toBe("oolschay");
+  });
+  test("has mix of cases, returns lowercase", () => {
+    expect(regExTranslate("guArD")).toBe("uArDgay");
+  });
+
+  test("contains punctuation", () => {
+    expect(regExTranslate("Flor.ida.man")).toBe("or.ida.manFlay");
+    expect(regExTranslate("doesn't")).toBe("oesn'tday");
+    expect(regExTranslate("could,not")).toBe("ould,notcay");
+  });
+
+  test("contains numbers or symbols", () => {
+    expect(regExTranslate("189")).toBe("189");
+    expect(regExTranslate("dill@gmail")).toBe("dill@gmail");
+  });
+
   test("contains a space", () => {
-    expect(translate("Have a wonderful day")).toBe(
-      "avehay away onderfulway ayday"
+    expect(regExTranslate("Have a wonderful day")).toBe(
+      "aveHay away onderfulway ayday"
     );
-    expect(translate("good golly apple")).toBe("oodgay ollygay appleway");
+    expect(regExTranslate("good golly apple")).toBe("oodgay ollygay appleway");
+  });
+
+  test("case sensitive, keeps case", () => {
+    expect(regExTranslate("caMelUppEr")).toBe("aMelUppErcay");
   });
 });
